90aa251dade8fb983d1e2408e24f9befd9c7eeba
Balram <bkushwah4321@gmail.com>
2026-03-21 14:02:23 +0530

    Balram

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const login = () => {
   return (
     <div className='text-black'>
-      this is the login page of elvia jewels
+      this is the login page of elvia jewels by Srishti.
     </div>
   )
 }
