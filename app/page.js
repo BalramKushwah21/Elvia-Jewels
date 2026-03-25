7a3e5f870340564434682317047dcbb197c8e8b4
Balram <bkushwah4321@gmail.com>
2026-03-25 22:50:35 +0530

    Balram

--- app/page.js
+++ app/page.js
@@ -1,23 +1,81 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import "./page.css";
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const slider = document.querySelector(".slider");
+
+let counter = 1;
+const rightbtn = () => {
+  // alert("right");
+  counter++;
+  slider.style.transform = `translateX(-${counter * 100}%)`;
+}
+
+const leftbtn = () => {
+  // alert("left");
+  counter--;
+  slider.style.transform = `translateX(${counter * 100}%)`;
+}
+
+
+
+
+
+
 
 const Home = () => {
   return (
-    <div>
-      <div
-        className="w- h-[40vh] bg-pink-300 bg-center m-2"
-          ></div>
+    <div className="main">
+      {/* this image slider */}
+    
+        <div className="container">
+            <button id="left" onClick={leftbtn}>
+            <svg className="left"
+              xmlns="http://www.w3.org/2000/svg"
+              height="48px"
+              viewBox="0 -960 960 960"
+              width="48px"
+              fill="#75FB4C"
+            >
+              <path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z" />
+            </svg>
+          </button>
 
+        <div className="frame">
+          <div className="slider">
+            <img src="slider/image1.png" className="image" />
+            <img src="slider/image2.png" className="image" />
+            <img src="slider/image3.png" className="image" />
+            <img src="slider/image4.png" className="image" />
+          </div>
+        </div>
+
+          <button id="right" onClick={rightbtn}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48px"
+              viewBox="0 -960 960 960"
+              width="48px"
+              fill="#75FB4C"
+            >
+              <path d="M686-450H160v-60h526L438-758l42-42 320 320-320 320-42-42 248-248Z" />
+            </svg>
+          </button>
+        </div>{/* Container end */}
+     
+      {/* category slider */}
       <div
         id="track"
         className="flex gap-4 my-10 justify-around overflow-hidden"
       >
         <div className="  flex-col text-center">
           <Image
+          alt="bracelet"
             className="rounded-4xl"
             src="/category/bracelet.png"
-            alt="Description"
             width={450}
             height={100}
           />
@@ -27,9 +85,9 @@ const Home = () => {
         </div>
         <div className="  flex-col text-center">
           <Image
+            alt="pendant"
             className=" rounded-4xl "
             src="/category/pendant.png"
-            alt="Description"
             width={450}
             height={100}
           />
@@ -39,9 +97,10 @@ const Home = () => {
         </div>
         <div className="  flex-col text-center">
           <Image
+            alt="earring"
             className="rounded-4xl"
             src="/category/earring.png"
-            alt="Description"
+            
             width={450}
             height={100}
           />
@@ -51,9 +110,11 @@ const Home = () => {
         </div>
         <div className="  flex-col text-center">
           <Image
+            alt="ring"
+
             className="rounded-4xl"
             src="/category/ring.png"
-            alt="Description"
+            
             width={450}
             height={100}
           />
@@ -61,9 +122,11 @@ const Home = () => {
         </div>
         <div className="  flex-col text-center">
           <Image
+            alt="anklet"
+
             className="rounded-4xl"
             src="/category/anklet.png"
-            alt="Description"
+           
             width={450}
             height={100}
           />
@@ -72,10 +135,11 @@ const Home = () => {
           </span>
         </div>
         <div className="  flex-col text-center">
-          <Image
+          <Image 
+            alt="bangle"
             className="rounded-4xl"
             src="/category/bangle.png"
-            alt="Description"
+    
             width={450}
             height={100}
           />
@@ -83,9 +147,10 @@ const Home = () => {
         </div>
         <div className="  flex-col text-center">
           <Image
+            alt="sets"
             className="rounded-4xl "
             src="/category/sets.png"
-            alt="Description"
+        
             width={450}
             height={100}
           />
@@ -93,9 +158,10 @@ const Home = () => {
         </div>
         <div className="  flex-col text-center">
           <Image
+            alt="meninsilver"
             className="rounded-4xl "
             src="/category/meninsilver.png"
-            alt="Description"
+         
             width={450}
             height={100}
           />
@@ -108,48 +174,49 @@ const Home = () => {
       <div id="image-container" className="flex-col gap-8">
         <div id="showimage" className="m-5">
           <Image
+          alt=""
             src="/features/demo1.jpeg"
-            alt="Description"
             width={1500}
             height={0}
           />
         </div>
         <div id="showimage" className="m-5">
           <Image
+            alt=""
+
             src="/features/demo1.jpeg"
-            alt="Description"
             width={1500}
             height={0}
           />
         </div>
         <div id="showimage" className="m-5">
           <Image
+              alt=""
             src="/features/demo1.jpeg"
-            alt="Description"
             width={1500}
             height={0}
           />
         </div>
         <div id="showimage" className="m-5">
           <Image
+            alt=""
             src="/features/demo1.jpeg"
-            alt="Description"
             width={1500}
             height={400}
           />
         </div>
         <div id="showimage" className="m-5">
           <Image
+            alt=""  
             src="/features/demo1.jpeg"
-            alt="Description"
             width={1500}
             height={400}
           />
         </div>
         <div id="showimage" className="m-5">
           <Image
+            alt=""
             src="/features/demo1.jpeg"
-            alt="Description"
             width={1500}
             height={400}
           />
--- componets/footer.js
+++ componets/footer.js
@@ -6,28 +6,28 @@ const footer = () => {
     
       <footer className="bg-white border-t px-12 py-16 grid md:grid-cols-4 gap-12">
         <div>
-          <h4 className="font-semibold text-xl mb-4">ELVIA JEWELS</h4>
-          <p className="text-gray-500">Premium jewellery brand delivering elegance and trust.</p>
+          <h4 className="font-semibold text-xl mb-4 text-blue-800">ELVIA JEWELS</h4>
+          <p className="text-black">Premium jewellery brand delivering elegance and trust.</p>
         </div>
 
         <div>
-          <h4 className="font-semibold mb-4">Quick Links</h4>
-          <p className="text-gray-500">Home</p>
-          <p className="text-gray-500">Shop</p>
-          <p className="text-gray-500">Contact</p>
+          <h4 className="font-semibold mb-4 text-blue-800">Quick Links</h4>
+          <p className="text-black">Home</p>
+          <p className="text-black">Shop</p>
+          <p className="text-black">Contact</p>
         </div>
 
         <div>
-          <h4 className="font-semibold mb-4">Collections</h4>
-          <p className="text-gray-500">Rings</p>
-          <p className="text-gray-500">Necklaces</p>
-          <p className="text-gray-500">Bracelets</p>
+          <h4 className="font-semibold mb-4 text-blue-800">Collections</h4>
+          <p className="text-black">Rings</p>
+          <p className="text-black">Necklaces</p>
+          <p className="text-black">Bracelets</p>
         </div>
 
         <div>
-          <h4 className="font-semibold mb-4">Contact</h4>
-          <p className="text-gray-500">support@elviajewels.com</p>
-          <p className="text-gray-500">+91 9876543210</p>
+          <h4 className="font-semibold mb-4 text-blue-800">Contact</h4>
+          <p className="text-black" >support@elviajewels.com</p>
+          <p className="text-black">+91 9876543210</p>
         </div>
       </footer>
   )
