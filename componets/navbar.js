8c5c07f6652777b462d6bc68a8dca70ba25cfaba
Balram <bkushwah4321@gmail.com>
2026-03-26 00:56:50 +0530

    register page added in navbar

--- componets/navbar.js
+++ componets/navbar.js
@@ -45,6 +45,12 @@ const navbar = () => {
         >
           Login
         </Link>
+        <Link
+          href="/auth/register"
+          className="px-3 py-2 text-white font-bold hover:bg-pink-300  rounded hover:text-black"
+        >
+          Register
+        </Link>
       </div>
     </nav>
   );
