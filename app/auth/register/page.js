811fdad2cd4c440fede97aeb4692e1fd99f38cdc
Yashita <yashitamalviya12@gmail.com>
2026-03-29 00:03:57 +0530

    add

--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -1,11 +1,57 @@
-import React from 'react'
+"use client";
+import React from "react";
+import "./register.css";
 
-const register = () => {
+export default function Register() {
   return (
-    <div className='text-black'>
-      this is the register page of elvia jewels
-    </div>
-  )
-}
+    <div className="main">
+      <div className="container">
 
-export default register
+        {/* LEFT SIDE */}
+        <div className="left">
+          <h1>REGISTER</h1>
+          <p>Create your account</p>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="right">
+          <div className="form-box">
+
+            <div className="avatar">👤</div>
+            <h2>REGISTER</h2>
+
+            <form>
+              <div className="input-box">
+                <span>👤</span>
+                <input type="text" placeholder="Full Name" />
+              </div>
+
+              <div className="input-box">
+                <span>📧</span>
+                <input type="email" placeholder="Email" />
+              </div>
+
+              <div className="input-box">
+                <span>🔒</span>
+                <input type="password" placeholder="Password" />
+              </div>
+
+              <div className="input-box">
+                <span>🔒</span>
+                <input type="password" placeholder="Confirm Password" />
+              </div>
+
+              <button>REGISTER</button>
+
+              <p>
+                Already have an account? <a href="/login">Login</a>
+              </p>
+            </form>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
