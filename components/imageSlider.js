d6d793656dc5612164ebee601817446efe3b138f
Srashti <srashti.b2020@gmail.com>
2026-04-03 11:06:30 +0530

    srashti

--- components/imageSlider.js
+++ components/imageSlider.js
@@ -12,9 +12,9 @@ export default function ImageSlider() {
     "/slider/image1.png",
     "/slider/image1.png",
     "/slider/image1.png",
-    
+    "/slider/image5.png",
   ];
-  
+
   const [index, setIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
 
@@ -24,7 +24,7 @@ export default function ImageSlider() {
 
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % images.length);
-    },2000);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, [isPaused, images.length]);
@@ -39,43 +39,42 @@ export default function ImageSlider() {
   };
 
   return (
-   <div
-  className="slider_wrapper"
-  onMouseEnter={() => setIsPaused(true)}
-  onMouseLeave={() => setIsPaused(false)}
->
-  <div
-    className="slider_track"
-    style={{
-      transform: `translateX(-${index * 100}%)`,
-    }}
-  >
-    {images.map((img, i) => (
-      <div className="slider_item" key={i}>
-        <img src={img} />
-        {/* <p className="slider_caption">Image {i + 1}</p> */}
+    <div
+      className="slider_wrapper"
+      onMouseEnter={() => setIsPaused(true)}
+      onMouseLeave={() => setIsPaused(false)}
+    >
+      <div
+        className="slider_track"
+        style={{
+          transform: `translateX(-${index * 100}%)`,
+        }}
+      >
+        {images.map((img, i) => (
+          <div className="slider_item" key={i}>
+            <img src={img} />
+            {/* <p className="slider_caption">Image {i + 1}</p> */}
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
 
-  <button className="img_slider_btn img_slider_btn_prev" onClick={prevSlide}>
-    ❮
-  </button>
-  <button className="img_slider_btn img_slider_btn_next" onClick={nextSlide}>
-    ❯
-  </button>
+      <button className="img_slider_btn img_slider_btn_prev" onClick={prevSlide}>
+        ❮
+      </button>
+      <button className="img_slider_btn img_slider_btn_next" onClick={nextSlide}>
+        ❯
+      </button>
 
-  <div className="slider_dots">
-    {images.map((_, i) => (
-      <span
-        key={i}
-        className={`slider_dot ${
-          i === index ? "slider_dot_active" : ""
-        }`}
-        onClick={() => setIndex(i)}
-      ></span>
-    ))}
-  </div>
-</div>
+      <div className="slider_dots">
+        {images.map((_, i) => (
+          <span
+            key={i}
+            className={`slider_dot ${i === index ? "slider_dot_active" : ""
+              }`}
+            onClick={() => setIndex(i)}
+          ></span>
+        ))}
+      </div>
+    </div>
   );
 }
