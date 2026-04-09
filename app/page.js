fadd76edbbcc4facf68e9f91cc82c456aceb3500
Yashita <yashitamalviya12@gmail.com>
2026-04-09 14:22:51 +0530

    'yashita

--- app/page.js
+++ app/page.js
@@ -80,7 +80,7 @@ export default async function Home() {
       </Image>
       <Image
       className={styles.posterImage}
-      src={"/Images/ElviaBoat.png"}
+      src={"https://www.giva.co/cdn/shop/files/first_access_Collection_Web_2.webp?v=1772101611&width=2000"}
       width={1000}
       height={100}
       alt="Poster"
