42f91531372f6eb62ffb9b6a532bb03e91ae4374
simrangupta-2006 <simranguptajsr2006@gmail.com>
2026-04-16 15:06:27 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@
 import { clear } from 'node:console';
 
  
-/** @type {import('next').NextConfig} */
+/*n@type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: ["10.250.47.234"],
+  allowedDevOrigins: ["10.24.197.74"],
     images: {
     remotePatterns: [
       {
