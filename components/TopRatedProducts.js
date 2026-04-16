63211970a947867052773ed77b92377c148be9b6
Yashita <yashitamalviya12@gmail.com>
2026-04-17 01:52:48 +0530

    yashita'

--- components/TopRatedProducts.js
+++ components/TopRatedProducts.js
@@ -23,9 +23,9 @@ export default async function TopRatedProducts() {
       {products.map((p) => (
         <div key={p.id} className={styles.card}>
           <img src={p.image} className={styles.image} />
-          <h3>{p.name}</h3>
-          <h3>{p.description}</h3>
-          <p>₹{p.price}</p>
+          <h3 className={styles.name}>{p.name}</h3>
+          <h3 className={styles.description}>{p.description}</h3>
+          <p className={styles.price}>₹{p.price}</p>
           <AddToCart productId={p.id } image={p.image} />
         </div>
       ))}
