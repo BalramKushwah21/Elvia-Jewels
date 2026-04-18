c96a03bc7bca078a595bf028dbe592e410a77cbf
Balram <bkushwah4321@gmail.com>
2026-04-18 22:58:46 +0530

    Balram

--- components/CollectionHeader.js
+++ components/CollectionHeader.js
@@ -6,17 +6,18 @@ export default function CollectionHeader({ title }) {
   return (
     <div className={styles.hero}>
       <div className={styles.overlay}>
-        <Image src={imgSrc} alt={title} fill style={{ objectFit: "cover" }} />
+        <Image src={imgSrc} alt={title} fill style={{ objectFit: "cover" }} loading="eager" priority />
       </div>
 
       <div className={styles.content}>
         {/* <h1 className={styles.title}>
           {title.charAt(0).toUpperCase() + title.slice(1)}
-        </h1> */}
+        </h1> 
 
         <p className={styles.subtitle}>Where elegance meets sparkle</p>
+        */}
 
-        <div className={styles.divider}></div>
+      
       </div>
     </div>
   );
