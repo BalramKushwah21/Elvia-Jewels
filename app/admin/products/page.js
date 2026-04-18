193bbd4100bf4d5720fe6f307ac84db9a762df63
Balram <bkushwah4321@gmail.com>
2026-04-18 15:13:01 +0530

    Now images are visible in edit page

--- app/admin/products/page.js
+++ app/admin/products/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 import styles from "./products.module.css";
+import Image from "next/image";
 
 export default async function ProductsPage() {
   const products = await prisma.product.findMany();
@@ -11,6 +12,7 @@ export default async function ProductsPage() {
       <div className={styles.productList}>
       {products.map((product) => (
         <div key={product.id} className={styles.card}>
+          <Image src={product.image} alt={product.name} className={styles.Image} width={200} height={200} />
           <h3 className={styles.productName}>{product.name}</h3>
           <p className={styles.price}>₹{product.price}</p>
 
