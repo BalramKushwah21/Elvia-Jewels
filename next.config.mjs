6621bef5eeebfea8c8787aa16ab9fbc042b1a83f
Balram <bkushwah4321@gmail.com>
2026-05-01 13:34:28 +0530

    Balram

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,7 @@ import { clear } from 'node:console';
 /*n@type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-<<<<<<< HEAD
   allowedDevOrigins: ["10.149.117.234"],
-=======
-  allowedDevOrigins: ["10.149.117.103"],
->>>>>>> dc98684bc12f405520f00e852dacf49a9bbf41d8
     images: {
     remotePatterns: [
       {
